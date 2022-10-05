06d54f3011a7e5bcaf291f70328ea5d5fb1ace1b
grandehe4rt <gabrieldias7200@gmail.com>
2022-10-04 22:33:51 -0300

    🐛 FIX: database connection more readable.

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,14 +1,14 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const URI = process.env.MONGODB_URI;
 
-export const databaseConnection = () => {
-    if (URI != undefined) {
-        mongoose.connect(URI);
+export const databaseConnection = async () => {
+    if (URI !== undefined) {
+        return await mongoose.connect(URI);
     } else {
-        throw new Error('Failed to connect to MongoDB database');
+        throw new Error("Failed to connect to MongoDB database");
     }
 };
